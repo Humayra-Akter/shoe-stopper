6ec6de4aec4c173c16229348a4bf037cc08188d0
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-21 22:57:44 +0600

    hero section

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -2,7 +2,16 @@ function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-        <h1>Shoe Stopper</h1>
+        <h1>Elevate Your Shoe Game at Shoe-Stopper</h1>
+        <h1>Where Every Step Counts</h1>
+        <p>
+          With Shoe-Stopper, every step becomes a celebration of individuality,
+          as we bring you the latest trends and timeless classics, all under one
+          roof.Explore our diverse range of shoes at Shoe-Stopper, where quality
+          meets affordability, and fashion meets function.Welcome to
+          Shoe-Stopper, where finding your perfect pair is not just a purchase,
+          but a journey towards confidence and self-expression.
+        </p>
       </div>
     </div>
   );
