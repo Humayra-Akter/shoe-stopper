95eee8a04ed35537865ef63aca71d89327d6a791
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-21 23:07:27 +0600

    hero section

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-        <h1>Elevate Your Shoe Game at Shoe-Stopper</h1>
-        <h1>Where Every Step Counts</h1>
-        <p>
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Elevate Your Shoe Game at Shoe-Stopper
+        </h1>
+        <h1 className="mt-10 md:mt-5 text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Where Every Step Counts
+        </h1>
+        <p className="mt-10 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           With Shoe-Stopper, every step becomes a celebration of individuality,
           as we bring you the latest trends and timeless classics, all under one
           roof.Explore our diverse range of shoes at Shoe-Stopper, where quality
@@ -12,6 +18,9 @@ function HeroSection() {
           Shoe-Stopper, where finding your perfect pair is not just a purchase,
           but a journey towards confidence and self-expression.
         </p>
+        <div className="mt-4">
+          <Link href={"/shoes"}>Explore More</Link>
+        </div>
       </div>
     </div>
   );
