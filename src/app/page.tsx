fa7236a278247f855e671c67bf422c0b63c18e34
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-26 13:06:37 +0600

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedShoes from "@/components/FeaturedShoes";
 import HeroSection from "@/components/HeroSection";
+import { MenShoes } from "@/components/MenShoes";
 import { Shoes } from "@/components/Shoes";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <MenShoes />
       <Shoes />
       <FeaturedShoes />
     </main>
