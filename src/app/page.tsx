6033248880f64c7a3f8264b064dd436ba5f89b8f
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-27 22:52:51 +0600

    card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedShoes from "@/components/FeaturedShoes";
 import HeroSection from "@/components/HeroSection";
 import { MenShoes } from "@/components/MenShoes";
 import { Shoes } from "@/components/Shoes";
+import { EvervaultCard } from "@/components/ui/evervault-card";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <div className="flex mt-28">
         <HeroSection />
         <MenShoes />
+      </div>
+      <div className="h-96 flex items-center justify-center border-2 border-slate-400">
+        <EvervaultCard />
       </div>
       <Shoes />
       <FeaturedShoes />
